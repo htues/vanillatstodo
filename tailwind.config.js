496b728b8b37c0a7398d4356ac46cbbf718a9aeb
Herbert Tamayo <hftamayo@gmail.com>
2024-08-25 13:48:11 -0600

    ensuring html files are also pre-processed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   mode: 'jit',
-  darkMode: "media", // or 'class'
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "media", 
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       fontFamily: {
