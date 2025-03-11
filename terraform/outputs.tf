e6d5c4991942641e34b1a5ffb486faf293381d86
Herbert Tamayo <hftamayo@gmail.com>
2025-03-11 13:58:52 -0600

    output tags for github actions

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "eks_cluster_name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
