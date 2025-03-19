a2783b8255690308e53d1ac6eb7f12bf135f3f8b
Herbert Tamayo <hftamayo@gmail.com>
2025-03-19 13:32:08 -0600

    manually code formatting on outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "eks_cluster_name" {
   description = "name of the EKS cluster"
-  value = aws_eks_cluster.main.name
+  value       = aws_eks_cluster.main.name
 }
 
 output "vpc_id" {
   description = "ID of the VPC"  
-  value = aws_vpc.main.id
+  value       = aws_vpc.main.id
 }
 
 output "eks_cluster_endpoint" {
   description = "Endpoint for EKS control plane"
-  value = aws_eks_cluster.main.endpoint
+  value       = aws_eks_cluster.main.endpoint
 }
