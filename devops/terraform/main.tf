ac9bb4e5f8e1999e32336675969cc98b08f914f4
Herbert Tamayo <hftamayo@gmail.com>
2025-03-24 13:59:59 -0600

    removing the creation of basic infra objects from main.tf

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -9,52 +9,6 @@ provider "aws" {
   }
 }
 
-# Create DynamoDB table for state locking
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "vanillatstodo-terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform State Lock Table"
-    Environment = "staging"
-  }
-}
-
-# Create S3 bucket for Terraform state
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "vanillatstodo-terraform-state"
-
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "staging"
-  }
-}
-
-# Enable versioning for state bucket
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Enable encryption for state bucket
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 # Create VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
