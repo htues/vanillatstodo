5ada6b6e49e26f62f23b11037658e9f799899cdb
Herbert Tamayo <hftamayo@gmail.com>
2025-03-31 15:24:36 -0600

    simplifying backend tf file

--- devops/terraform/00_state/backend.tf
+++ devops/terraform/00_state/backend.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "vanillatstodo-terraform-state"
-    key            = "staging/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "vanillatstodo-terraform-state-lock"
+    # Backend configuration will be provided during init
   }
 }
