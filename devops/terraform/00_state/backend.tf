018055d3bf4fab15c3c4226a14eac22ff62ffea4
Herbert Tamayo <hftamayo@gmail.com>
2025-04-01 14:31:51 -0600

    updating tf resources strategy

--- devops/terraform/00_state/backend.tf
+++ devops/terraform/00_state/backend.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 1.0.0"
-  
-  backend "s3" {}  # Empty block - configuration will be provided via CLI
+  backend "s3" {
+    bucket         = "vanillatstodo-terraform-state"
+    key            = "staging/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "vanillatstodo-terraform-state-lock"
+  }
 }
