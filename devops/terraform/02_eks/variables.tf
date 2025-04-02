05a0adee77dcdaa73a16334759832a80d4ed8782
Herbert Tamayo <hftamayo@gmail.com>
2025-04-02 16:15:39 -0600

    trying to automate enviro vars def

--- devops/terraform/02_eks/variables.tf
+++ devops/terraform/02_eks/variables.tf
@@ -27,6 +27,18 @@ variable "aws_region" {
   default     = "us-east-2"
 }
 
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "vanillatstodo-cluster"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version for EKS cluster"
+  type        = string
+  default     = "1.27"
+}
+
 variable "log_retention_days" {
   description = "Number of days to retain EKS cluster logs"
   type        = number
