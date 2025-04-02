d21c9b083b9710b05f6d9bd82813b042db15826f
Herbert Tamayo <hftamayo@gmail.com>
2025-04-02 12:10:02 -0600

    expanding outputs.tf vars

--- devops/terraform/01_network/outputs.tf
+++ devops/terraform/01_network/outputs.tf
@@ -3,6 +3,11 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
+output "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block  # Fix: Changed from .id to .cidr_block
+}
+
 output "subnet_ids" {
   description = "IDs of the created subnets"
   value = [
@@ -11,7 +16,28 @@ output "subnet_ids" {
   ]
 }
 
-output "vpc_cidr" {
-  description = "CIDR block of the VPC"
-  value       = aws_vpc.main.id
+output "subnet_cidrs" {
+  description = "CIDR blocks of the created subnets"
+  value = {
+    subnet_a = aws_subnet.subnet_a.cidr_block
+    subnet_b = aws_subnet.subnet_b.cidr_block
+  }
+}
+
+output "route_table_id" {
+  description = "ID of the main route table"
+  value       = aws_route_table.main.id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "availability_zones" {
+  description = "Availability zones used by the subnets"
+  value = {
+    subnet_a = aws_subnet.subnet_a.availability_zone
+    subnet_b = aws_subnet.subnet_b.availability_zone
+  }
 }
