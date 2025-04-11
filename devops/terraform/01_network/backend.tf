072f70e30c2268761e9e41ecd18c789b3b185c04
Herbert Tamayo <hftamayo@gmail.com>
2025-04-11 08:13:38 -0600

    updating tf version and enhacing security

--- devops/terraform/01_network/backend.tf
+++ devops/terraform/01_network/backend.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.10.0"
 
   backend "s3" {
     bucket         = "vanillatstodo-terraform-state"
-    key            = "staging/network.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
-    dynamodb_table = "vanillatstodo-terraform-state-lock"
+    force_path_style = false
   }
 }
