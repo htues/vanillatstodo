f407e1a88fc3ed45241251bad71199042086e932
Herbert Tamayo <hftamayo@gmail.com>
2025-04-11 14:34:05 -0600

    dealing with eks bugs part2

--- devops/terraform/02_eks/backend.tf
+++ devops/terraform/02_eks/backend.tf
@@ -4,9 +4,11 @@ terraform {
 
 data "terraform_remote_state" "network" {
   backend = "s3"
+  workspace = "staging"
   config = {
-    bucket = "vanillatstodo-terraform-state"
-    key    = "staging/network.tfstate"
-    region = "us-east-2"
+    bucket  = "vanillatstodo-terraform-state"
+    key     = "staging/network.tfstate"
+    region  = "us-east-2"
+    encrypt = true
   }
 }
