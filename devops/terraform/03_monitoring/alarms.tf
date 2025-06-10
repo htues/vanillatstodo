23673639abbfb298fdea2750cf2043356c41b2cb
Herbert Tamayo <hftamayo@gmail.com>
2025-06-10 12:58:56 -0600

    applying tagging strategy on monitoring package

--- devops/terraform/03_monitoring/alarms.tf
+++ devops/terraform/03_monitoring/alarms.tf
@@ -1,6 +1,6 @@
 # CloudWatch Alarms
 resource "aws_cloudwatch_metric_alarm" "node_count" {
-  alarm_name          = "${var.environment}-${var.cluster_name}-node-count"
+  alarm_name          = "${var.project_name}-${var.cluster_name}-node-count"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "2"
   metric_name         = "cluster_node_count"
@@ -14,8 +14,7 @@ resource "aws_cloudwatch_metric_alarm" "node_count" {
     ClusterName = data.aws_eks_cluster.main.name
   }
 
-  tags = {
-    Environment = var.environment
-    ManagedBy   = "terraform"
-  }
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-${var.cluster_name}-node-count-alarm"
+  })
 }
