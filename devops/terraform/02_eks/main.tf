110e64c628f14650c178bc0eeedb88a871ec7861
Herbert Tamayo <hftamayo@gmail.com>
2025-06-23 12:35:33 -0600

    fixing eks subnets for concatenate public and private ones

--- devops/terraform/02_eks/main.tf
+++ devops/terraform/02_eks/main.tf
@@ -45,7 +45,7 @@ resource "aws_eks_cluster" "main" {
   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
   vpc_config {
-    subnet_ids              = data.terraform_remote_state.network.outputs.subnet_ids
+    subnet_ids              = local.all_subnets
     endpoint_private_access = true
     endpoint_public_access  = true
     security_group_ids      = [aws_security_group.eks_cluster.id]
