db1e9b2851dc1c631257cac81ae427d7d1c5666e
Herbert Tamayo <hftamayo@gmail.com>
2025-08-29 10:17:35 -0600

    adding worker nodes in eks main.tf

--- devops/terraform/02_eks/main.tf
+++ devops/terraform/02_eks/main.tf
@@ -56,3 +56,39 @@ resource "aws_eks_cluster" "main" {
     Version = "1.31"
   })
 }
+
+# IAM role for EKS Node Group
+data "aws_iam_role" "eks_nodegroup" {
+  name = "${var.environment}-eks-nodegroup-role"
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "workers" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "workers"
+  node_role_arn   = data.aws_iam_role.eks_nodegroup.arn
+  subnet_ids      = local.private_subnets
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_eks_cluster.main,
+  ]
+
+  tags = merge(local.common_tags, {
+    Name = "${var.environment}-${var.project_name}-workers"
+  })
+}
